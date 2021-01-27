834384f393d6ff9ac1dd191be8c810459124ed91
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-01-27 17:20:40 -0400

    Update PizzaTime.py
    Made comments to better understand
    #issue: Isn't running through loop enough times to deliver all 9 pizzas. Loop only runs 5 times

--- PizzaTime.py
+++ PizzaTime.py
@@ -1,20 +1,21 @@
 class Piza:
-    def __init__(self, ingredients, ID):
-        self.score = 0
-        self.ingredients = ingredients
+    def __init__(self, ingredients, ID, ingCount):
+        self.ingredients = ingredients#list of ingredients text
         self.ID = ID
+        self.ingCount = ingCount
         teamSize = -1
+        self.score = 0
     
     def ingredientScoreCalc(self):
         for item in self.ingredients:
             self.score += 1/ingredientDict[item]
             
-    def setTeam(teamSize):
+    def setTeam(self, teamSize):
         self.teamSize = teamSize
             
             
     def __str__(self):
-        strin = "\nIngredients: " + str(self.ingredients) + "\nScore: " + str(self.score)
+        strin = "\nIngredients: " + str(self.ingredients) + "\nScore: " + str(self.score) + "\nTeam: " + str(self.teamSize)
         return strin
     
     
@@ -25,17 +26,15 @@ def output():
 ingredientDict = {} #ingredients : score
 initialData = [] # numPizza, numTwo, numThree, numFour
 pizzas = []
-pizzasDelivered = 0
 
 
 with open("a_example", "r") as file:
     fileData = file.readlines()
-    #initialData = fileData[0].split()
     initialData = [int(x) for x in fileData[0].split()]
-    #index = 0 #0 index for Piza id
-    for index,data in enumerate(fileData[1:]):
-        ingredients = data.split()[1:]
-        pizza = Piza(ingredients, index)
+    for index,lineData in enumerate(fileData[1:]):#(index,lineData) 
+        ingCount = lineData.split()[0:1]
+        ingredients = lineData.split()[1:]
+        pizza = Piza(ingredients, index, ingCount[0])
         pizzas.append(pizza)
         
         for ing in ingredients:
@@ -44,21 +43,25 @@ with open("a_example", "r") as file:
                 
             else:
                 ingredientDict[ing] = 1
-        index += 1        
+        index += 1 
+        
+pizzaCount = initialData[0]        
 for piza in pizzas:
     piza.ingredientScoreCalc()
     
-count = int(initialData[0])
 pIndex = 0
 for i in range(3,0,-1): 
-    while count >= i+1 and int(initialData[i]) > 0:
-        pizzasDelivered += 1
-        initialData[i] -= 1
+    while pizzaCount >= i+1 and int(initialData[i]) > 0:
+        initialData[i] -= 1 #decrement count of teams at index
+        pizzaCount -= 1 #decrement count of pizzas
         
-        for j in range(pIndex, pIndex+i+1):
-            pizzas[pIndex].setTeam(i+1)
-        
-        pIndex += 1
+        for j in range(pIndex, pIndex+i+1):#j = index for pizzas running from current pizza and accumulated 
+            try:
+                pizzas[j].setTeam(i+1)#now all Pizas will know which team they belong to
+                pIndex += 1
+            
+            except IndexError:#no more pizzas
+                break
     
         
         
