6c5ee94dbf7c9f70db6737d922ff58fe3c27c180
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-02-09 19:30:23 -0400

    Implementation
    Took basic description and implemented recursive function with helper function.
    TODO: Implement pizzaList creator in main function

--- PizzaTime.py
+++ PizzaTime.py
@@ -17,19 +17,43 @@ class Piza:
     def __str__(self):
         strin = "Index: " + str(self.ID)+ "\nIngredients: " + str(self.ingredients) + "\nScore: " + str(self.score) + "\nTeam: " + str(self.teamSize)
         return strin    
+
+#neededForTeam = how many pizzas still need to be delivered for the current team 0 and up
+#firstPizza = index of pizza you start looking from in pizzaList    
+def depthFirst(neededForTeam, firstPizza):#recursive
+    #grab PizzaList assuming format [(type,count),...]
+    #also use initialData
+    if neededForTeam == 0:
+        for i in range(2, 5):#go through teams, delivering i pizzas
+            if initialData[i-1] > 0:
+                initialData[i-1] -= 1 #decrement team to reserve position
+                depthFirst(i, 0)#this is set to 0 to allow all pizzas to be picked up when the combinations start at index 1 and above
+                initialData[i-1] += 1 #to come back recursively
+    else:
+        nextPizza = findFirstNonEmpty(firstPizza)
+        while nextPizza >= 0:#at this node, you can pick many different pizzas, so we're essentially finding every combination of them
+            temp = pizzaList[nextPizza]
+            pizzaList[nextPizza] = (temp[0], temp[1]-1)#replace the count (tuples are immutable)
+            depthFirst(neededForTeam-1,firstPizza+1)#found one pizza for this set of combinations for this node, let's find another one
+            pizzaList[nextPizza] = temp
+            nextPizza = findFirstNonEmpty(nextPizza+1)#to find the next pizza in line for our while loop
     
-def depthFirst(highScore, pizzas, initialData, index):#recursive
-    if pizzas.len = 0 or (initial[1] = 0 and initialData[2] = 0 and initialData[3] = 0):#also needs to account for having enough pizzas for the team size
-        return highScore
+def findFirstNonEmpty(pizzaIndex):
+    while pizzaIndex < len(pizzaList):
+        if pizzaList[pizzaIndex][1] > 0:
+            return pizzaIndex
+        else:
+            pizzaIndex += 1
+    
+    return -1
         
-    pizza = pizzas[index]
-    pizzas[index] = None
-    
+    #function (pizzaStructure, index, number of pizzas to be delivered to team)
     #potentially implement a list of tuples [(pizza type, count),(),()]
     #Go through the teams, deliver the number of pizzas to the team
     #Decrement the count, increment a highScore, go until there are either not enough pizzas or no teams
     #Never deliver same time of pizza to a team
     #Attempt combination of every type of pizza delivered to a team, then next team (decrement team count and pizza type count)
+    #Implementation: recursive function takes in # pizzas still selected for a team, and the first pizza that it's allowed to select 
     
     
 def depthFirstNaive (highScore, pizzas, initialData):#shift array in every combination of 
@@ -55,6 +79,7 @@ def sequentialPizzaAssignment(pizzaCount, pizzas,initialData):
 ingredientDict = {} #ingredients : score
 initialData = [] #[ numPizza, numTwo, numThree, numFour]
 pizzas = []
+pizzaList = []
 
 with open("./a_example", "r") as file:
     fileData = file.readlines()
@@ -86,7 +111,7 @@ for piza in pizzas:
 
 highScore = 0
 
-depthFirst(highScore, pizzas, initialData, 0)
+depthFirst(0,0)
 
 
 
